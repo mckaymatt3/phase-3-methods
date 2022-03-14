8f08028fda4fd2e182514cc4db3a04d73c0490b3
mckaymatt3 <mckaymatt@outlook.com>
2022-03-14 16:42:30 -0400

    commit tree 4c732cb78bd58c5605b45fbb1631ced0b0fadd29

--- methods.rb
+++ methods.rb
@@ -1,1 +1,33 @@
 # Your code here!
+
+#   First 
+#   You should be able to call this function with no arguments and see its output in the terminal:
+#   greetProgrammer();
+#   => "Hello, programmer!"
+
+def greet_programmer
+    puts "Hello, programmer!"
+    "Hello, programmer!"
+end
+
+def greet(name)
+    puts "Hello, #{name}!"
+end
+
+def greet_with_default(name="programmer")
+    puts "Hello, #{name}!"
+end
+
+def add(num1, num2)
+    puts num1 + num2
+    num1 + num2
+end
+
+def halve(num1)
+    if num1.class != Integer
+        return nil
+    end
+
+    puts num1
+        num1/2
+end
